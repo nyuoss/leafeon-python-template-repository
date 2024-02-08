d7f67e142b2d2d1f3e55f3c776e7371fcfd59f7c
Dorothy <dorothyzhang825@gmail.com>
2024-02-08 18:35:15 -0500

    added model component

--- source/main.py
+++ source/main.py
@@ -1,2 +1,8 @@
+from source.utility.component import add
+
+
 def hello():
     print("Hello World!")
+    result = add(1, 2)
+    result += 1
+    print("Result incremented is {result}")
--- source/tests/test_main.py
+++ source/tests/test_main.py
@@ -3,4 +3,5 @@ import pytest
 import source.main as mn
 
 def test_hello():
+    mn.hello()
     assert (2 + 2 == 4)
